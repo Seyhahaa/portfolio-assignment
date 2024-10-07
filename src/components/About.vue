6f34846598d46d62dfb57df62bdc22f955df5c33
Seyhahaa <jbseyha@gmail.com>
2024-10-07 13:19:04 +0700

    img aboutPage

--- src/components/About.vue
+++ src/components/About.vue
@@ -13,7 +13,7 @@
       </div>
       <div v-else>
         <div class='fixed mt-[90px]'>
-            <img src='https://images.pexels.com/photos/6929027/pexels-photo-6929027.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' class='h-screen object-cover'  />
+            <img src='/about.webp' class='h-screen object-cover'  />
         </div>
       </div>
     </div>
