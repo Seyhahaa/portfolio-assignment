499842d7911ab4a892fd424e669ccbac05e2c171
Seyhahaa <jbseyha@gmail.com>
2024-10-07 16:26:40 +0700

    Update Footer

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div className="w-[90px] h-screen border-r-[1px] fixed flex flex-col items-center justify-around mt-[90px] bg-white">
+    <div className="w-[90px] h-screen border-r-[1px] fixed flex flex-col items-center justify-around mt-[90px] bg-white ">
         <h2 className='-rotate-90 tracking-widest text-[1.5rem]'>HOMEPAGE</h2>
         <div className='flex flex-col gap-7 mb-10'>
             <v-icon name="bi-github" scale="2" class='cursor-pointer hover:scale-110 hover:text-green-600 transition-all ease-in-out' />
